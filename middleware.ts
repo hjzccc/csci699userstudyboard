f1c7e1c5519de7762df7ce5396ab63fad69de223
h21565897 <h21565897@gmail.com>
2024-01-06 20:27:09 -0800

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -9,16 +9,14 @@ export function middleware(req: NextRequest) {
   const isStaticResource = /\.\w+$/.test(pathName);
   console.log(pathName);
   console.log(isStaticResource);
-  if (
-    pathName != "/admin/login" &&
-    pathName != "/api/admin/login" &&
-    (pathName.startsWith("/api/admin") || pathName.startsWith("/admin"))
-  ) {
-    if (
-      !req.cookies.get("admin") ||
-      req.cookies.get("admin")?.value != adminpass
-    ) {
-      return new NextResponse("UnAuthorized", { status: 401 });
+  if (pathName.startsWith("/api/admin") || pathName.startsWith("/admin")) {
+    if (pathName != "/admin/login" && pathName != "/api/admin/login") {
+      if (
+        !req.cookies.get("admin") ||
+        req.cookies.get("admin")?.value != adminpass
+      ) {
+        return new NextResponse("UnAuthorized", { status: 401 });
+      }
     }
   } else if (
     !isStaticResource &&
